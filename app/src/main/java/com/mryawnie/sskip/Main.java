4d09e57233573d9d3aa887878237a0ea0c87a36c
MrYawnie <jokeri_fani1@hotmail.com>
2016-07-01 22:29:55 +0300

    Initial commit

---
+++ app/src/main/java/com/mryawnie/sskip/Main.java
@@ -0,0 +1,60 @@
+package com.mryawnie.sskip;
+
+import java.lang.reflect.Method;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by MrYawnie on 30.6.2016.
+ */
+public class Main implements IXposedHookLoadPackage, IXposedHookInitPackageResources {
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam lpparam) throws Throwable {
+        // Enables unlimited skips
+        if (!lpparam.packageName.equals("com.spotify.music"))
+            return;
+
+        lpparam.res.setReplacement("com.spotify.music", "bool", "is_tablet", true);
+
+
+    }
+
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam packageParam) throws Throwable {
+        if (packageParam.packageName.equalsIgnoreCase("com.spotify.music")) {
+            XposedBridge.log("We are in Spotify!");
+            //find LoadedFlags class
+            Class<?> loadedFlagsCls = XposedHelpers.findClass("com.spotify.mobile.android.service.feature.LoadedFlags", packageParam.classLoader);
+            //iterate through all methods to find our flag class
+            Class<?> flagCls = null;
+            for (Method m : loadedFlagsCls.getMethods()) {
+                if (m.getName().equalsIgnoreCase("a") && m.getParameterTypes().length == 1) {
+                    flagCls = m.getParameterTypes()[0];//actually it's just a lucky guess
+                }
+            }
+            //failsafe check and nice info
+            if (flagCls == null) {
+                XposedBridge.log("Flag class not found!");
+                return;
+            }
+            //hook this method so we can modify flag state
+            XposedHelpers.findAndHookMethod(loadedFlagsCls, "a", flagCls, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    //get flag name
+                    String flag = (String) XposedHelpers.getObjectField(param.args[0], "b");
+                    //disable chosen flags
+                    if (flag.equalsIgnoreCase("ads") || flag.equalsIgnoreCase("on-demand") || flag.equalsIgnoreCase("shuffle_restricted")) {
+                        param.setResult(false);
+                    }
+                }
+            });
+        }
+    }
+}
+
---
+++ app/src/main/java/com/mryawnie/sskip/MainActivity.java
@@ -0,0 +1,33 @@
+package com.mryawnie.sskip;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openBrowser(View view){
+
+        //Get url from tag
+        String url = (String)view.getTag();
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        //pass the url to intent data
+        intent.setData(Uri.parse(url));
+
+        startActivity(intent);
+    }
+}
